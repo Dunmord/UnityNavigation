cdb8c5d4dcc12cebfb66235a07d4f511a9c84696
GamePipelineDev-at-387221157085 <javierfalconc@gmail.com>
2020-06-14 21:44:46 -0700

    Add NavMeshComponents example
    This examples shows how to use the NavMeshSurface and the NavMeshModifier.

---
+++ Assets/Scripts/NavMeshAgentController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshAgentController : MonoBehaviour
+{
+    public GameObject target;
+    public NavMeshAgent agent;
+    private void Awake()
+    {
+        if (!agent)
+        {
+            agent = GetComponent<NavMeshAgent>();
+        }
+    }
+    private void Update()
+    {
+        agent.SetDestination(target.transform.position);
+    }
+}
